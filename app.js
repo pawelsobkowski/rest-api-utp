97343722d69c31c08c48a3927c3759012b638350
Pawel <ssopel10@gmail.com>
2020-05-14 19:29:47 +0200

    minor fix

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.use(signupRoute);
 app.use(signinRoute);
 app.use(eventsRoute);
